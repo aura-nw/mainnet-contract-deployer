443bd693adb973bb1943e6644fcc428700375abb
doquockhanhan <doquockhanhan@gmail.com>
2022-08-26 15:54:29 +0700

    add more error messages

--- types/errors.ts
+++ types/errors.ts
@@ -9,7 +9,7 @@ export enum ErrorMessage {
 	DUPLICATED_LOGIN = 'user.duplicated.login',
 	DUPLICATED_EMAIL = 'user.duplicated.email',
 	DELETE_ITSELF = 'user.delete.itself',
-	CONTRACT_NOT_FOUND = 'This contract does not exist',
+	CONTRACT_NOT_FOUND = 'This contract(s) does not exist or is not verified',
 	INSUFFICIENT_FUNDS = 'Insufficient funds',
 	SIMULATE_TX_FAIL = 'Simulate transaction failed',
 	REQUEST_SUCCESSFUL = 'Contract(s) deployment request successful',
@@ -18,6 +18,8 @@ export enum ErrorMessage {
 	INVALID_CREDENTIALS = 'Invalid email or password, please try again',
 	NEW_PASS_REQUIRED = 'User must change password in first login time',
 	LOGIN_SUCCESSFUL = 'Login successful',
+	CONTRACT_ALREADY_UPLOADED = 'Contract(s) already uploaded on Mainnet',
+	CONTRACT_ALREADY_REQUESTED = 'Contract(s) already being requested to upload',
 }
 
 export const ErrorCode = {
@@ -35,4 +37,6 @@ export const ErrorCode = {
 	INVALID_PASSWORD: 'E005',
 	NEW_PASS_REQUIRED: 'E006',
 	INVALID_CREDENTIALS: 'E007',
+	CONTRACT_ALREADY_UPLOADED: 'E008',
+	CONTRACT_ALREADY_REQUESTED: 'E009',
 };
